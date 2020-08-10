f859b6efdd6599ebe9145e02749c7059ee20bb8f
hesau hugo <hesau@forsell.com.br>
2020-08-10 11:38:07 -0300

    commit

---
+++ ToledoVB/ToledoVB/Form1.Designer.vb
@@ -0,0 +1,134 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.Inicio = New System.Windows.Forms.Button()
+        Me.Parar = New System.Windows.Forms.Button()
+        Me.transferir = New System.Windows.Forms.Button()
+        Me.label1 = New System.Windows.Forms.Label()
+        Me.Saida_Tela = New System.Windows.Forms.Label()
+        Me.SerialPort1 = New System.IO.Ports.SerialPort(Me.components)
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.Panel1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'Inicio
+        '
+        Me.Inicio.Location = New System.Drawing.Point(151, 3)
+        Me.Inicio.Name = "Inicio"
+        Me.Inicio.Size = New System.Drawing.Size(75, 23)
+        Me.Inicio.TabIndex = 1
+        Me.Inicio.Text = "Inicio"
+        Me.Inicio.UseVisualStyleBackColor = True
+        '
+        'Parar
+        '
+        Me.Parar.Location = New System.Drawing.Point(313, 3)
+        Me.Parar.Name = "Parar"
+        Me.Parar.Size = New System.Drawing.Size(75, 23)
+        Me.Parar.TabIndex = 2
+        Me.Parar.Text = "Parar"
+        Me.Parar.UseVisualStyleBackColor = True
+        '
+        'transferir
+        '
+        Me.transferir.Location = New System.Drawing.Point(181, 105)
+        Me.transferir.Name = "transferir"
+        Me.transferir.Size = New System.Drawing.Size(186, 37)
+        Me.transferir.TabIndex = 6
+        Me.transferir.Text = "Transferir"
+        Me.transferir.UseVisualStyleBackColor = True
+        '
+        'label1
+        '
+        Me.label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 20.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.label1.Location = New System.Drawing.Point(145, 51)
+        Me.label1.Name = "label1"
+        Me.label1.Size = New System.Drawing.Size(86, 40)
+        Me.label1.TabIndex = 7
+        Me.label1.Text = "Peso"
+        '
+        'Saida_Tela
+        '
+        Me.Saida_Tela.Font = New System.Drawing.Font("Microsoft Sans Serif", 20.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Saida_Tela.Location = New System.Drawing.Point(237, 51)
+        Me.Saida_Tela.Name = "Saida_Tela"
+        Me.Saida_Tela.Size = New System.Drawing.Size(160, 40)
+        Me.Saida_Tela.TabIndex = 8
+        Me.Saida_Tela.Text = "Sem Leitura"
+        '
+        'SerialPort1
+        '
+        Me.SerialPort1.BaudRate = 4800
+        Me.SerialPort1.PortName = "COM14"
+        Me.SerialPort1.RtsEnable = True
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.TextBox1.Location = New System.Drawing.Point(0, 153)
+        Me.TextBox1.Multiline = True
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.ScrollBars = System.Windows.Forms.ScrollBars.Both
+        Me.TextBox1.Size = New System.Drawing.Size(550, 243)
+        Me.TextBox1.TabIndex = 9
+        '
+        'Panel1
+        '
+        Me.Panel1.Controls.Add(Me.Inicio)
+        Me.Panel1.Controls.Add(Me.Parar)
+        Me.Panel1.Controls.Add(Me.Saida_Tela)
+        Me.Panel1.Controls.Add(Me.transferir)
+        Me.Panel1.Controls.Add(Me.label1)
+        Me.Panel1.Dock = System.Windows.Forms.DockStyle.Top
+        Me.Panel1.Location = New System.Drawing.Point(0, 0)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(550, 153)
+        Me.Panel1.TabIndex = 10
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(550, 396)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Panel1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.Panel1.ResumeLayout(False)
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Private WithEvents Inicio As Button
+    Private WithEvents Parar As Button
+    Private WithEvents transferir As Button
+    Private WithEvents label1 As Label
+    Private WithEvents Saida_Tela As Label
+    Private WithEvents SerialPort1 As IO.Ports.SerialPort
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Panel1 As Panel
+End Class
---
+++ ToledoVB/ToledoVB/Form1.vb
@@ -0,0 +1,154 @@
+﻿Imports System
+Imports System.Collections.Generic
+Imports System.ComponentModel
+Imports System.Data
+Imports System.Drawing
+Imports System.Linq
+Imports System.Text
+Imports System.Windows.Forms
+Imports System.IO.Ports
+Imports System.Runtime.InteropServices
+Imports System.Diagnostics
+Imports System.Configuration
+
+Public Class Form1
+    Private RxString As String
+    Dim count As Integer
+    Private Sub Inicio_Click(sender As Object, e As EventArgs) Handles Inicio.Click
+        SerialPort1.Open()
+        MsgBox("porta abriu")
+        If SerialPort1.IsOpen Then
+            Inicio.Enabled = False
+            Parar.Enabled = True
+            transferir.Enabled = True
+        End If
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        btnLer_Click()
+
+        SerialPort1 = New SerialPort
+        SerialPort1.BaudRate = 4800
+        SerialPort1.PortName = "COM14"
+        SerialPort1.RtsEnable = True
+
+        If SerialPort1.IsOpen Then
+            SerialPort1.Close()
+            Inicio.Enabled = True
+            Parar.Enabled = False
+        End If
+
+        SerialPort1.PortName = ConfigurationManager.AppSettings("PortaSerial")
+        SerialPort1.BaudRate = 4800
+        SerialPort1.DataBits = 8
+        SerialPort1.DtrEnable = True
+        SerialPort1.StopBits = StopBits.One
+        SerialPort1.DataBits = 7
+        SerialPort1.NewLine = Environment.NewLine
+        SerialPort1.ReadTimeout = 500
+        SerialPort1.WriteTimeout = 500
+        MsgBox("Load")
+        count = 0
+    End Sub
+
+    Private Sub btnLer_Click()
+
+
+
+        Dim fluxoTexto As IO.StreamReader
+
+        Dim linhaTexto As String
+        Dim caminho As String = "D:\source\TOLEDOVB\TOLEDO_STRING_PESO.txt"
+
+
+        If IO.File.Exists(caminho) Then
+
+            fluxoTexto = New IO.StreamReader(caminho)
+
+            linhaTexto = fluxoTexto.ReadLine
+
+            Dim status As String = String.Empty
+
+            While linhaTexto <> Nothing
+                If Len(linhaTexto.Trim) > 1 Then
+                    If linhaTexto.Contains(ChrW(2)) Then
+                        linhaTexto = linhaTexto.Replace(ChrW(2), "@")
+                        If Len(linhaTexto) > 4 Then
+                            status = "INSTÁVEL: "
+                            If Mid(linhaTexto.Replace(ChrW(2), "@"), 3, 1) = "0" Then status = "ESTÁVEL: "
+                            linhaTexto = linhaTexto.Remove(0, 4)
+                            If Len(linhaTexto) > 6 Then
+                                linhaTexto = linhaTexto.Remove(6).Replace(" ", "0")
+                                TextBox1.Text += status & linhaTexto & vbCrLf
+                            End If
+                        End If
+                    End If
+                End If
+                linhaTexto = fluxoTexto.ReadLine
+            End While
+
+            fluxoTexto.Close()
+
+        Else
+
+            MessageBox.Show("Arquivo não existe")
+
+        End If
+
+    End Sub
+
+
+    Private Sub SerialPort1_DataReceived(sender As Object, e As SerialDataReceivedEventArgs) Handles SerialPort1.DataReceived
+        Dim peso As String = SerialPort1.ReadExisting()
+        Dim status As String = "INSTÁVEL: "
+        If Len(peso.Trim) > 1 Then
+            If peso.Contains(ChrW(2)) Then
+                peso = peso.Replace(ChrW(2), "@")
+                If Len(peso) > 4 Then
+                    If Mid(peso.Replace(ChrW(2), "@"), 3, 1) = "0" Then status = "ESTÁVEL: "
+                    peso = peso.Remove(0, 4)
+                    If Len(peso) > 6 Then
+                        peso = peso.Remove(6).Replace(" ", "0")
+                        TextBox1.Text += status & peso & vbCrLf
+                        Saida_Tela.Text = peso
+                    End If
+                End If
+            End If
+        End If
+    End Sub
+
+    Private Sub Parar_Click(sender As Object, e As EventArgs) Handles Parar.Click
+        If SerialPort1.IsOpen Then
+            SerialPort1.Close()
+            Inicio.Enabled = True
+            Parar.Enabled = False
+            transferir.Enabled = False
+            Saida_Tela.Text = "Sem Leitura"
+        End If
+    End Sub
+
+    'Private Sub Saida_Tela_TextChanged(sender As Object, e As KeyPressEventArgs) Handles Saida_Tela.TextChanged
+    '    If Not SerialPort1.IsOpen Then Return
+    '    Dim buff As Char() = New Char(0) {}
+    '    buff(0) = e.KeyChar
+    '    SerialPort1.Write(buff, 0, 1)
+    '    e.Handled = True
+    'End Sub
+
+    Private Sub Form1_FormClosed(sender As Object, e As FormClosedEventArgs) Handles MyBase.FormClosed
+        If SerialPort1.IsOpen Then
+            SerialPort1.Close()
+        End If
+    End Sub
+
+    Private Sub transferir_Click(sender As Object, e As EventArgs) Handles transferir.Click
+        'Dim currentProcess As Process = Process.GetCurrentProcess()
+        'Dim processes As Process() = Process.GetProcessesByName("saplogon")
+
+        'For Each p As Process In processes
+        '    Dim pFoundWindow As IntPtr = p.MainWindowHandle
+        '    SetForegroundWindow(pFoundWindow)
+        '    SendKeys.SendWait(RxString)
+        'Next
+    End Sub
+End Class
